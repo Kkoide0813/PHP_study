1e7a85fc247579ac5662a31b6fc64bd6c3720fec
KoideKeiji <qwejks0201@gmail.com>
2025-09-22 20:05:34 +0900

    基礎文法05 数値の演算

--- main.php
+++ main.php
@@ -1,11 +1,10 @@
 <?php
-// コンソール => PHP_EOLと"\n"は同じ
-// ブラウザ上 => "<br />"
-echo 10 . "\n";
-echo -8 . PHP_EOL;
-echo 2.5 . PHP_EOL;
-echo 1000000 . PHP_EOL;
-echo 1_000_000 . PHP_EOL;
-echo 1.2e-3 . PHP_EOL;
-echo 1.2e3 . PHP_EOL;
+echo 10 + 3 . PHP_EOL;
+echo 10 - 3 . PHP_EOL;
+echo 10 * 3 . PHP_EOL;
+echo 10 ** 3 . PHP_EOL;
+echo 10 / 3 . PHP_EOL;
+echo 10 % 3 . PHP_EOL;
+echo 10 + 2 * 3 . PHP_EOL;
+echo (10 + 2) * 3 . PHP_EOL;
 ?>
