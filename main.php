793bb5bdd8e8dadb718eab04d7cd72cadd25fd51
KoideKeiji <qwejks0201@gmail.com>
2025-09-22 20:22:05 +0900

    基礎文法06 変数

--- main.php
+++ main.php
@@ -1,10 +1,10 @@
 <?php
-echo 10 + 3 . PHP_EOL;
-echo 10 - 3 . PHP_EOL;
-echo 10 * 3 . PHP_EOL;
-echo 10 ** 3 . PHP_EOL;
-echo 10 / 3 . PHP_EOL;
-echo 10 % 3 . PHP_EOL;
-echo 10 + 2 * 3 . PHP_EOL;
-echo (10 + 2) * 3 . PHP_EOL;
+// echo 150 * 120 . PHP_EOL;
+// echo 150 * 130 . PHP_EOL;
+// echo 150 * 140 . PHP_EOL;
+
+$price = 150;
+echo $price * 120 . PHP_EOL;
+echo $price * 130 . PHP_EOL;
+echo $price * 140 . PHP_EOL;
 ?>
