664e08fab09c070d5e02ddff4987ec6b55323fa9
KoideKeiji <qwejks0201@gmail.com>
2025-09-22 22:14:03 +0900

    基礎文法07 変数に値を代入

--- main.php
+++ main.php
@@ -1,9 +1,16 @@
 <?php
-// echo 150 * 120 . PHP_EOL;
-// echo 150 * 130 . PHP_EOL;
-// echo 150 * 140 . PHP_EOL;
+// $price = 150;
+// echo $price * 120 . PHP_EOL;
+// echo $price * 130 . PHP_EOL;
+// echo $price * 140 . PHP_EOL;
+
+// $price = 151;
+// $price = $price + 1;
 
 $price = 150;
+$price += 1;
+// $price ++;
+// $price --;
 echo $price * 120 . PHP_EOL;
 echo $price * 130 . PHP_EOL;
 echo $price * 140 . PHP_EOL;
