cc44c6b106921f16afe91375eac4f9deea6ee7d9
KoideKeiji <qwejks0201@gmail.com>
2025-09-22 19:51:35 +0900

    基礎文法04

--- main.php
+++ main.php
@@ -1,4 +1,11 @@
 <?php
-echo "MAMPの設定が完了しました<br />";
-echo "次はGitHubでバージョン管理可能にします\n";
+// コンソール => PHP_EOLと"\n"は同じ
+// ブラウザ上 => "<br />"
+echo 10 . "\n";
+echo -8 . PHP_EOL;
+echo 2.5 . PHP_EOL;
+echo 1000000 . PHP_EOL;
+echo 1_000_000 . PHP_EOL;
+echo 1.2e-3 . PHP_EOL;
+echo 1.2e3 . PHP_EOL;
 ?>
