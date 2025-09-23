1e59aed5b92add84d8e3deb18c70f50553f384a9
KoideKeiji <qwejks0201@gmail.com>
2025-09-23 13:27:10 +0900

    定数の扱い

--- main.php
+++ main.php
@@ -1,17 +1,14 @@
 <?php
-// $price = 150;
-// echo $price * 120 . PHP_EOL;
-// echo $price * 130 . PHP_EOL;
-// echo $price * 140 . PHP_EOL;
-
-// $price = 151;
-// $price = $price + 1;
-
 $price = 150;
-$price += 1;
-// $price ++;
-// $price --;
-echo $price * 120 . PHP_EOL;
-echo $price * 130 . PHP_EOL;
-echo $price * 140 . PHP_EOL;
+// // $rate = 1.1;
+// const RATE =1.1;
+// echo $price * 120 * RATE . PHP_EOL;
+// echo $price * 130 * RATE . PHP_EOL;
+// echo $price * 140 * RATE . PHP_EOL;
+
+$price ++;
+const RATE = 1.2;
+echo $price * 120 * RATE . PHP_EOL;
+echo $price * 130 * RATE . PHP_EOL;
+echo $price * 140 * RATE . PHP_EOL;
 ?>
