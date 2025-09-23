9ef3a2da1a6676dd2b25a7028b8ac22ab9297c30
KoideKeiji <qwejks0201@gmail.com>
2025-09-23 18:23:08 +0900

    for文反復処理

--- main.php
+++ main.php
@@ -1,41 +1,22 @@
 <?php
-echo "Signal? ";
-$signal = trim(fgets(STDIN));
-
-switch ($signal) {
-    case "red":
-        echo "stop" . PHP_EOL;
-        break;
-    case "green":
-        echo "go" . PHP_EOL;
-        break;
-    case "yellow":
-        echo "slow down" . PHP_EOL;
-        break;
-    // どれにも当てはまらない時
-    default:
-        echo "wrong signal" . PHP_EOL;
-        break;
+// 九九の四の段をfor文で反復処理
+for ($i = 1; $i <= 9; $i++) {
+    echo 4 * $i . PHP_EOL;
 }
-
 
 /* 
 
 実行時
 
-
-koidekeiji@koidemegumitsugunoMacBook-Air htdocs % php main.php                                                                   
-Signal? red
-stop
-koidekeiji@koidemegumitsugunoMacBook-Air htdocs % php main.php
-Signal? green
-go
-koidekeiji@koidemegumitsugunoMacBook-Air htdocs % php main.php
-Signal? yellow
-slow down
-koidekeiji@koidemegumitsugunoMacBook-Air htdocs % php main.php
-Signal? aaa
-wrong signal
-% 
+% php main.php                                                                   
+4
+8
+12
+16
+20
+24
+28
+32
+36
 
 */
