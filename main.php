0c1a5dcc9551d8ee3ce126970fbc7fa3e6188e1e
KoideKeiji <qwejks0201@gmail.com>
2025-09-23 15:18:27 +0900

    属性・文字数確認ver_dump, 除去trimについて

--- main.php
+++ main.php
@@ -1,23 +1,32 @@
 <?php
-$name = $_POST['name'] ?? ''; // nameがなければ’’を使用
-?>
+echo "Your name?";
+// $name = fgets(STDIN);
+$name = trim(fgets(STDIN));
+// var_dump($name); // 変数nameの属性確認
+echo $name . PHP_EOL;
 
-<!DOCTYPE html>
-<html lang="ja">
+/* 
+Taroの後にEnterキー押した分も文字数としてカウントされてしまう
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>入力フォーム</title>
-</head>
+% php main.php
+Your name?Taro
+string(5) "Taro
 
-<body>
-  <form method="post">
-    <label>お名前は: <input type="text" name="name"></label>
-    <button type="submit">送信</button>
-  </form>
-  <?php if ($name !== ''): ?>
-    <p>私の名前は <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?> です。</p>
-  <?php endif; ?>
-</body>
-</html>
+=> そこで不要な値を除去するためにtrimを使用
+
+% php main.php
+Your name?Taro
+string(4) "Taro"
+
+4文字で正しく表示された
+
+% php main.php
+Your name?Taro
+Taro
+
+% php main.php
+Your name?Taro
+Taro
+% 
+
+" */
