322dc8cd7add9ae53e55e74d4b901be61dc27225
KoideKeiji <qwejks0201@gmail.com>
2025-09-23 22:35:33 +0900

    関数 引数にデフォルト値を設定する場合

--- main.php
+++ main.php
@@ -1,23 +1,22 @@
 <?php
-function sum($a, $b) {
-    return $a + $b;
+/* 商品の税込合計金額を計算する getTotal ()という関数を作る
+引数で単価、数量、税率という意味で仮引数を設定して、合計金額としてそれらを掛け合わせたものを返す */
+
+// デフォルトでrateを1.1にする
+function getTotal($price, $amount, $rate = 1.1) { // デフォルト値を設定する変数は一番後ろにしないとエラーになる
+    return $price * $amount * $rate;
 }
 
-function printSum($a, $b) {
-    echo $a + $b . PHP_EOL;
-    // return Null; が省略されている
-}
-
-// 呼び出し元
-echo sum(3, 7) . PHP_EOL;
-var_dump(printSum(10, 3));
+echo getTotal(1000, 3) . PHP_EOL;
+echo getTotal(500, 2) . PHP_EOL;
+echo getTotal(200, 30, 1.08) . PHP_EOL;
 
 /* 
 実行時
 
-% php main.php                                                                       
-10
-13
-NULL
+% php main.php
+3300
+1100
+6480
 
 */
