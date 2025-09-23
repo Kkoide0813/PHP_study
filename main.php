763e952202bedd712fa9b47c4462ee974abef5eb
KoideKeiji <qwejks0201@gmail.com>
2025-09-23 13:57:23 +0900

    標準入力の受付方法

--- main.php
+++ main.php
@@ -1,14 +1,12 @@
 <?php
-$price = 150;
-// // $rate = 1.1;
-// const RATE =1.1;
-// echo $price * 120 * RATE . PHP_EOL;
-// echo $price * 130 * RATE . PHP_EOL;
-// echo $price * 140 * RATE . PHP_EOL;
+echo "Your name" . PHP_EOL;
+$name = fgets(STDIN);
+echo "私の名前は{$name}です" . PHP_EOL;
 
-$price ++;
-const RATE = 1.2;
-echo $price * 120 * RATE . PHP_EOL;
-echo $price * 130 * RATE . PHP_EOL;
-echo $price * 140 * RATE . PHP_EOL;
-?>
+/* ユーザーに文字列を入力し、変数で受け取ることができる　standard input 標準入力の略
+標準入力はターミナルで使用すると使える。VScodeのコンソールではxdebugのせいで使えない。
+% php main.php
+Your name
+小出
+私の名前は小出
+です */
