700fc89f5f90f4543032bc5428dded574c8ecf8a
KoideKeiji <qwejks0201@gmail.com>
2025-09-23 22:24:18 +0900

    関数 returnしない場合_2

--- main.php
+++ main.php
@@ -5,16 +5,19 @@ function sum($a, $b) {
 
 function printSum($a, $b) {
     echo $a + $b . PHP_EOL;
+    // return Null; が省略されている
 }
 
+// 呼び出し元
 echo sum(3, 7) . PHP_EOL;
-printSum(10, 3);
+var_dump(printSum(10, 3));
 
 /* 
 実行時
 
-% php main.php
+% php main.php                                                                       
 10
 13
+NULL
 
 */
