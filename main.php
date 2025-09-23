0e54d40bd4e2236c7a9add8f9bfde9193022af18
KoideKeiji <qwejks0201@gmail.com>
2025-09-23 15:44:06 +0900

    Stringからintへの型キャスト

--- main.php
+++ main.php
@@ -1,16 +1,43 @@
 <?php
-echo "Your name? => ";
-$name = trim(fgets(STDIN));
-// 文字列の連結
-echo "Hello {$name}" . PHP_EOL; 
-echo "Hello " . $name . PHP_EOL;
+echo "価格はいくら? => ";
+$price = (int) fgets(STDIN);
+echo "税込" . $price * 1.1 . "円です" . PHP_EOL; // 文字列の連結と試してみる
+// var_dump($price);
 
 /* 
+
+実行時
+% php main.php
+価格はいくら? => 100
+税込110円です
+
+*/
+
+
+
+/* 
+==========================
+echo "Price? ";
+$price = trim(fgets(STDIN));
+var_dump($price);
+
+実行時
+
+% php main.php 
+Price? 100
+string(3) "100" // 文字列型になっている
+
+==========================
+
+<?php
+echo "Price? ";
+$price = (int) fgets(STDIN); // int型に変換
+var_dump($price);
+
 実行時
 
 % php main.php
-Your name? => Taro
-Hello Taro
-Hello Taro
+Price? 100
+int(100) // 正しく変換された int型はtrimなくても最後の改行は除去される
 
 */
