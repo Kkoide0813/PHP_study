1098c5a5932042373ba3a54ca2ff47bedd841279
KoideKeiji <qwejks0201@gmail.com>
2025-09-23 21:47:57 +0900

    論理演算子 A&&B , A||B

--- main.php
+++ main.php
@@ -1,19 +1,24 @@
 <?php
-// 三項演算子 スコアが80以上ならA,それ以外はB
-echo "Score?";
-$score = (int) fgets(STDIN);
-$result = $score > 80 ? "A" : "B" ;
-echo $result . PHP_EOL;
+// 論理演算子 英語が 80 点以上、なおかつ数学が 80 点以上だったら A 判定
+echo "English?";
+$english = (int)fgets(STDIN);
+echo "Math?";
+$math = (int)fgets(STDIN);
+
+// 読みづらい
+// if ($english >= 80) {
+//     if ($math >= 80) {
+//         echo "A" . PHP_EOL;
+//     }
+// } else echo "失格";
+
+if ($english >= 80 && $math >=80) { // && アンパサンド
+    echo "A";
+} else {
+    echo "B";
+}
 
 
 /* 
-
 実行時
-% php main.php                                                                                            
-Score?80
-B
-% php main.php
-Score?90
-A
-
 */
