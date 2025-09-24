019ebb25cfa3d15ab19e62453fe250c01827b520
KoideKeiji <qwejks0201@gmail.com>
2025-09-24 15:01:17 +0900

    名前付き引数での呼び出し方法

--- main.php
+++ main.php
@@ -1,28 +1,15 @@
 <?php
-/* 商品の税込合計金額を計算する getTotal ()という関数を作る
-引数で単価、数量、税率という意味で仮引数を設定して、合計金額としてそれらを掛け合わせたものを返す
-
-追加：合計100個以上だったら、税金分を無料にする
+/* 
+printMessage() という関数名で、$from と $to を渡したら、それに応じたメッセージを表示してみましょう。
+$to の人に向けて、$from の人から挨拶をすることを想定して、このようにしてみます。
 */
 
-
-// デフォルトでrateを1.1にする
-function getTotal($price, $amount, $rate = 1.1) { // デフォルト値を設定する変数は一番後ろにしないとエラーになる
-    // 合計100個以上だったら、税金分を無料
-    if ($amount >= 100) {
-        return $price * $amount;
-    }else {
-        return $price * $amount * $rate;
-    }
+function printMessage($from, $to) {
+    return $from . "さんから" . $to . "さんへのメッセージです";    
 }
 
-echo getTotal(1000, 3) . PHP_EOL;
-echo getTotal(500, 2) . PHP_EOL;
-echo getTotal(200, 30, 1.08) . PHP_EOL;
-echo getTotal(200, 100, 1.08) . PHP_EOL;
-echo getTotal(200, 100) . PHP_EOL;
 
-/* 
-実行時
-
-*/
+echo printMessage("Koide", "Tanaka") . PHP_EOL;
+// $マークいらない。順序変えても正常に動作する
+echo printMessage(from:"Koide", to:"Tanaka") . PHP_EOL;
+echo printMessage(to:"Tanaka", from: "Koide") . PHP_EOL;
