f2ed7e9e84bf28e65be6755f02799f26d99722f1
KoideKeiji <qwejks0201@gmail.com>
2025-09-24 22:17:43 +0900

    データ構造編 配列 調べ方 count,max,min,array_sum

--- main.php
+++ main.php
@@ -1,34 +1,10 @@
 <?php
-/*  データ構造編   */
+/*  配列の調べ方 */
 $scores = [70, 90, 80];
-echo $scores[0] . PHP_EOL; // 70
 
-// 値の上書き
-$scores[1] = 95;
-echo $scores[1] . PHP_EOL; // 95
+echo count($scores) . PHP_EOL; // 要素数 3
+echo max($scores) . PHP_EOL; // 最大値 90
+echo min($scores) . PHP_EOL; // 最小値 70
+echo array_sum($scores) . PHP_EOL; // 合計値 240
 
-// 全ての値を表示
-print_r($scores) . PHP_EOL; // var_dumpより見やすい
-/* 
-Array
-(
-    [0] => 70
-    [1] => 95
-    [2] => 80
-)
-*/
-
-// var_dump($scores) . PHP_EOL;
-/* 
-
-見づらい
-
-array(3) {
-  [0]=>
-  int(70)
-  [1]=>
-  int(95)
-  [2]=>
-  int(80)
-}
-*/
+echo array_sum($scores) / count($scores) . PHP_EOL; // 平均 80
