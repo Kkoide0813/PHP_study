90cc343ce7c5f65b93c78f1511c3d94071996dbc
KoideKeiji <qwejks0201@gmail.com>
2025-09-24 21:54:10 +0900

    関数編 型付け_3 ?を使った型付け

--- main.php
+++ main.php
@@ -1,14 +1,19 @@
 <?php
-/*  union型  */
+/*  ?を使った型付け  */
 
 // 厳格な型付け
 declare(strict_types=1);
 
 // 商品idを渡すと、P-商品idという商品コードをかえす処理
-function getProductCode(int|string $id): string // 仮引数をint,string両方受け取れる書き方＝union型と呼ぶ
+function getProductCode(int $id): ?string // NULL|string の簡略記法
+// function getProductCode(int $id): NULL|string
 {
+    // idが100以上ならNULLを返す
+    if($id >= 100) {
+        return var_dump(NULL);
+    }
     return "P-{$id}";
 }
 
-echo getProductCode(80) . PHP_EOL;
-echo getProductCode("banana") . PHP_EOL;
+echo getProductCode(80) . PHP_EOL; // 商品コードを返す
+echo getProductCode(100) . PHP_EOL; // NULL
