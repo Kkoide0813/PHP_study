598589cc9390a1df230154c72e6129b1662b727f
KoideKeiji <qwejks0201@gmail.com>
2025-09-25 16:57:37 +0900

    データ構造編 配列の要素を各変数に代入 list(変数1,変数2,変数3)= 配列変数

--- main.php
+++ main.php
@@ -1,30 +1,25 @@
 <?php
-/* 配列の並び替え sort(), rsort(), asort(), array_reverse() */
+/* 配列の要素を変数に代入 */
 $scores = [70, 90, 80];
 
-// echo "配列の並べ替え 小さい順 sort()" . PHP_EOL;
-// sort($scores); 
-// print_r($scores);
+$firstScore = $scores[0];
+$secondScore = $scores[1];
+$thirdScore = $scores[2];
 
-// echo "配列の並べ替え 大きい順 rsort()" . PHP_EOL;
-// rsort($scores);
-// print_r($scores);
+// 冗長な書き方
+echo $firstScore . PHP_EOL;
+echo $secondScore . PHP_EOL;
+echo $thirdScore . PHP_EOL;
 
-// echo "元の配列のキーを維持したまま並び替え asort()" . PHP_EOL;
-// asort($scores);
-// print_r($scores);
+// 別の書き方
+list($firstScore, $secondScore, $thirdScore) = $scores; // list()内の変数にそれぞれ代入できる
+echo $firstScore . PHP_EOL;
+echo $secondScore . PHP_EOL;
+echo $thirdScore . PHP_EOL;
 
-// echo " シャッフル shuffle()" . PHP_EOL; // 実行されるたびにシャッフルされる
-// shuffle($scores);
-// print_r($scores);
+// 別の書き方
+[$firstScore, $secondScore, $thirdScore] = $scores; // [ ]は配列ではない
+echo $firstScore . PHP_EOL;
+echo $secondScore . PHP_EOL;
+echo $thirdScore . PHP_EOL;
 
-echo " 要素を逆順に並べ替え array_reverse()" . PHP_EOL;
-
-array_reverse($scores); // array_reverse()元の配列をそのままの状態で、”戻り値で”逆にした新たな配列を返す
-print_r(array_reverse($scores));
-// $reversed = array_reverse($scores); // 変数に代入してリファクタリング
-// print_r($reversed);
-
-
-// array_reverse($scores); // これだと元の配列がそのまま出力される
-// print_r($scores);
