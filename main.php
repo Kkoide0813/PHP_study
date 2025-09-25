4ba01eb932f1f7d1db288c400e2c8aa1819e9f1d
KoideKeiji <qwejks0201@gmail.com>
2025-09-25 18:03:55 +0900

    データ構造編 配列の書き換え foreach(),array_map()の2通りの方法

--- main.php
+++ main.php
@@ -1,22 +1,39 @@
 <?php
-/* 配列の要素を変数に代入_2 foreach(配列変数 as 変数) */
-$scores = [70, 80, 90];
-// foreach($scores as $value) {
-//     echo "Score: {$value}" . PHP_EOL; 
-// }
+/* 各スコアをそれぞれ5点プラスしたい */
 
 /* 
-Score: 70
-Score: 80
-Score: 90
-*/
+foreachで空の配列に入れる方法
+*/ 
 
-foreach($scores as $key => $value) { // 値だけでなく、キーも取り出す方法
-    echo "Score[{$key}]: {$value}" . PHP_EOL;
+$scores = [70, 80, 90];
+$updatedScores = [];
+
+foreach($scores as $value){
+    $updatedScores[] = $value + 5; // 配列の末尾に追加する書き方
 }
 
+print_r($updatedScores);
+
+// =======================================================
+
 /* 
-Score[0]: 70
-Score[1]: 80
-Score[2]: 90
+array_map(関数, $scores) を使う方法
 */
+
+// 関数addFiveを変数$addFiveとして呼び出したいので、呼び出し元は無名関数を使わないといけない。
+$scores = [70, 80, 90];
+$addFive = function($n){
+        return $n + 5;
+    };
+
+$updatedScores = array_map($addFive, $scores);
+print_r($updatedScores);
+
+// =======================================================
+//もっと短くアロー関数で書くことも可能
+$addFive = fn($n) => $n + 5;
+$scores = [70, 80, 90];
+
+// $updatedScores = array_map($addFive, $scores);
+$updatedScores = array_map(fn($n) => $n + 5, $scores); // アロー関数の処理を直接書いた場合
+print_r($updatedScores);
