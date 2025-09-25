39be4feaa4e052b76e92a5d557ac3e8d415d548d
KoideKeiji <qwejks0201@gmail.com>
2025-09-25 10:39:28 +0900

    データ構造編 配列 調べ方_2 in_array, array_search

--- main.php
+++ main.php
@@ -1,10 +1,11 @@
 <?php
 /*  配列の調べ方 */
-$scores = [70, 90, 80];
+$names = ["Taro","Jiro" ,"Saburo"];
 
-echo count($scores) . PHP_EOL; // 要素数 3
-echo max($scores) . PHP_EOL; // 最大値 90
-echo min($scores) . PHP_EOL; // 最小値 70
-echo array_sum($scores) . PHP_EOL; // 合計値 240
+// 関数in_array() 一致する文字列を検索
+var_dump(in_array("Taro", $names)); // 存在する true
+var_dump(in_array("shiro", $names)); // 存在しない false
 
-echo array_sum($scores) / count($scores) . PHP_EOL; // 平均 80
+// array_search() 何番目に存在するか検索
+echo array_search("Jiro", $names) . PHP_EOL; // 1
+
