8e970dbc80a5a29a97a804e00aef265743678534
KoideKeiji <qwejks0201@gmail.com>
2025-09-26 17:54:22 +0900

    クラス インスタンス変数、クラスメソッド

--- main.php
+++ main.php
@@ -1,28 +1,28 @@
 <?php
 // クラスプロパティ
 class User {
-    // インスタンスプロパティ・・・インスタンス変数
+
     public $name;
     public $score;
-    // クラスプロパティ・・・クラス変数
-    // インスタンスごとに値を持たせるのではなく、クラス全体で1つの値だけを管理したいので、staticとする
-    public static $count = 0; // インスタンスの作成回数
+    private static $count = 0; // インスタンスの作成回数
 
     public function __construct($name, $score)
     {
         $this -> name = $name;
         $this -> score = $score;
-        User::$count++; // static変数の書き方
+        User::$count++;
     }
-    // いくつインスタンスが作成されたかを表示
 
-
+    // 個々のインスタンスに紐づかないように、staticメソッドにする
+    public static function getUserCount(){
+        return User::$count;
+    }
 }
 
-$count = 0;
 $user1 = new User("Taro", 70);
-$count++;
 $user2 = new User("Jiro", 90);
-$count++;
 
-echo User::$count . PHP_EOL;
+// User::$count++; // インスタンス化せずにカウントされてしまう。クラス外でカウントできないようにする。
+// echo User::$count . PHP_EOL; // $countをprivateにするとこっちもエラーが出てしまう。getterを設定する
+
+echo User::getUserCount() . PHP_EOL; // staticメソッドの呼び出し方
