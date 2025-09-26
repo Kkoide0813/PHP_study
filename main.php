93459a8b00157aa1d73967d02f5376b174fd8355
KoideKeiji <qwejks0201@gmail.com>
2025-09-26 15:31:00 +0900

    クラス カプセル化_1

--- main.php
+++ main.php
@@ -17,12 +17,31 @@ class User {
     public function getInfo(){
         return "{$this -> name}, {$this -> score}";
     }
+
+    // setScoreメソッドの作成
+    public function setScore($score){
+        if($score >=0 && $score <=100 ){
+            $this->score = $score;
+        } else {
+            echo "Invalid score!" . PHP_EOL; // 更新せずリターン
+            return;
+        }
+    }
 }
 
 // インスタンス
-// もっとすっきりさせる
 $user1 = new User("Taro", 70);
 $user2 = new User("Jiro", 90);
+
+// $user1->score = 900; // 直接プロパティの値が更新できてしまうのは良くない。
+// /*
+// Taro, 900
+// Jiro, 90
+// */
+
+// setScore()というメソッドを介してプロパティにアクセスする
+$user1->setScore(900);
+$user1->setScore(60);
 
 echo $user1->getInfo() . PHP_EOL;
 echo $user2->getInfo() . PHP_EOL;
