877a8496ad08362c47d5f3af82d47914fbf3a7a3
KoideKeiji <qwejks0201@gmail.com>
2025-09-26 17:01:17 +0900

    クラス インスタンス変数、クラス変数=static クラス名::変数

--- main.php
+++ main.php
@@ -1,32 +1,28 @@
 <?php
+// クラスプロパティ
 class User {
+    // インスタンスプロパティ・・・インスタンス変数
     public $name;
-    private $score;
+    public $score;
+    // クラスプロパティ・・・クラス変数
+    // インスタンスごとに値を持たせるのではなく、クラス全体で1つの値だけを管理したいので、staticとする
+    public static $count = 0; // インスタンスの作成回数
 
     public function __construct($name, $score)
     {
         $this -> name = $name;
         $this -> score = $score;
+        User::$count++; // static変数の書き方
     }
+    // いくつインスタンスが作成されたかを表示
 
-    public function getInfo(){
-        return "{$this -> name}, {$this -> score}, {$this -> getResult()}";
-    }
 
-    private function getResult(){
-        if($this->score >=80){
-            return "Pass";
-        } else {
-            return "Fail";
-        }
-    }
 }
 
-// インスタンス
+$count = 0;
 $user1 = new User("Taro", 70);
+$count++;
 $user2 = new User("Jiro", 90);
+$count++;
 
-echo $user1->getInfo() . PHP_EOL;
-echo $user2->getInfo() . PHP_EOL;
-// echo $user1->getResult() . PHP_EOL; // クラス外なので、エラー
-// echo $user2->getResult() . PHP_EOL; // クラス外なので、エラー
+echo User::$count . PHP_EOL;
