facb793c94fd39cca35d3c8686d0f2a49d946d96
KoideKeiji <qwejks0201@gmail.com>
2025-09-26 15:00:19 +0900

    クラス メソッド、コンストラクタ

--- main.php
+++ main.php
@@ -5,15 +5,20 @@ class User {
     // プロパティ
     public $name;
     public $score;
+
+    // メソッド・・・クラス内で定義する関数
+    // コンストラクタ・・・newする時に実行される特殊なメソッド
+    public function __construct($name, $score)
+    {
+        $this -> name = $name;
+        $this -> score = $score;
+    }
 }
 
 // インスタンス
-$user1 = new User();
-$user1 -> name = "Taro";
-$user1 -> score = 70;
-$user2 = new User();
-$user2 -> name = "Jiro";
-$user2 -> score = 90;
+// もっとすっきりさせる
+$user1 = new User("Taro", 70);
+$user2 = new User("Jiro", 90);
 
 echo $user1 -> name . PHP_EOL;
 echo $user1 -> score . PHP_EOL;
